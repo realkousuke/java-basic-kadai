989f02976bd9ec22678fd8bb217244461ed5c792
realkousuke <147561305+realkousuke@users.noreply.github.com>
2023-11-07 08:25:32 +0900

    Add files via upload

---
+++ kadai_004/Score_Chapter04.java
@@ -0,0 +1,18 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+	public static void main(String[] args) {
+		System.out.println(10);
+		System.out.println(20);
+		System.out.println(30);
+		System.out.println(40);
+		System.out.println(50);
+		System.out.println(60);
+		System.out.println(70);
+		System.out.println(80);
+		System.out.println(90);
+		System.out.println(100);
+		
+		System.out.println( (10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90 + 100) / 10);
+	}
+}
