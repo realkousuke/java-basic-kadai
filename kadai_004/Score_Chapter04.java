47c4b29d29a1191eab944a753da52f39bd25f658
realkousuke <147561305+realkousuke@users.noreply.github.com>
2023-11-07 15:39:55 +0900

    Add files via upload

--- kadai_004/Score_Chapter04.java
+++ kadai_004/Score_Chapter04.java
@@ -2,17 +2,17 @@ package kadai_004;
 
 public class Score_Chapter04 {
 	public static void main(String[] args) {
-		System.out.println(10);
-		System.out.println(20);
-		System.out.println(30);
-		System.out.println(40);
-		System.out.println(50);
-		System.out.println(60);
-		System.out.println(70);
-		System.out.println(80);
-		System.out.println(90);
-		System.out.println(100);
+		System.out.println("Aさんの点数は" + 10 + "点");
+		System.out.println("Bさんの点数は" + 20 + "点");
+		System.out.println("Cさんの点数は" + 30 + "点");
+		System.out.println("Dさんの点数は" + 40 + "点");
+		System.out.println("Eさんの点数は" + 50 + "点");
+		System.out.println("Fさんの点数は" + 60 + "点");
+		System.out.println("Gさんの点数は" + 70 + "点");
+		System.out.println("Hさんの点数は" + 80 + "点");
+		System.out.println("Iさんの点数は" + 90 + "点");
+		System.out.println("Jさんの点数は" + 100 + "点");
 		
-		System.out.println( (10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90 + 100) / 10);
+		System.out.println("テストの平均点は" + (10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90 + 100) / 10 + "点");
 	}
 }
