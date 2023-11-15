7e9f71d66a6fef9707f2bc1b0d7557fe082aef3f
realkousuke <147561305+realkousuke@users.noreply.github.com>
2023-11-15 21:38:04 +0900

    Add files via upload

---
+++ kadai_017/KatoTaro_Chapter17.java
@@ -0,0 +1,12 @@
+package kadai_017;
+
+public class KatoTaro_Chapter17 extends Kato_Chapter17 {
+	
+	public void setGiveName() {
+		this.givenName = "太郎";
+	}
+	
+	public void eachIntroduce() {
+		System.out.println("Javaが得意です");
+	}
+}
