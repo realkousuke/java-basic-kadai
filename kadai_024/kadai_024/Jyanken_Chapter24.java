c0103c4c8ccf636f0f5830a3ddc1e065cbfb4251
realkousuke <147561305+realkousuke@users.noreply.github.com>
2023-11-20 20:17:31 +0900

    Add files via upload

---
+++ kadai_024/kadai_024/Jyanken_Chapter24.java
@@ -0,0 +1,72 @@
+package kadai_024;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Jyanken_Chapter24 {
+	public String getMyChoice() {
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			System.out.println("自分のじゃんけんの手を入力しましょう");
+			System.out.println("グーはrockのrを入力しましょう");
+			System.out.println("チョキはscissorsのsを入力しましょう");
+			System.out.println("パーはpaperのpを入力しましょう");
+			
+			String input = scanner.next();
+			
+		if(input.equals("r") || input.equals("s") || input.equals("s"))  {
+			System.out.println(input + "はじゃんけんの手ではありません");
+		} else {
+			scanner.close();
+			return input;
+		}
+		}
+		
+	}
+	
+	public String getRandom() {
+		String[] rpsArray = {"r", "p", "s"};
+		
+		String Rondom = rpsArray[(int) Math.floor(Math.random() * 3)];
+		return Rondom;
+	}
+	
+	public void playGame(String myChoice, String random) {
+
+	    HashMap<String, String> output = new HashMap<String, String>();
+	    output.put("r", "グー");
+	    output.put("s", "チョキ");
+	    output.put("p", "パー");
+
+	    System.out.println("自分の手は" + output.get(myChoice) + "対戦相手の手は" + output.get(random));
+		
+		
+		if(output.get(myChoice).equals("r") && output.get(random).equals("r")) {
+			System.out.println("自分の勝ちです");
+		}
+		if(output.get(myChoice).equals("r") && output.get(random).equals("s")) {
+			System.out.println("自分の勝ちです");
+		}
+		if(output.get(myChoice).equals("r") && output.get(random).equals("p")) {
+			System.out.println("自分の負けです");
+		}
+		if(output.get(myChoice).equals("s") && output.get(random).equals("r")) {
+			System.out.println("自分の負けです");
+		}
+		if(output.get(myChoice).equals("s") && output.get(random).equals("s")) {
+			System.out.println("あいこです");
+		}
+		if(output.get(myChoice).equals("s") && output.get(random).equals("p")) {
+			System.out.println("自分の勝ちです");
+		}
+		if(output.get(myChoice).equals("p") && output.get(random).equals("r")) {
+			System.out.println("自分の勝ちです");
+		}
+		if(output.get(myChoice).equals("p") && output.get(random).equals("s")) {
+			System.out.println("自分の負けです");
+		}
+		if(output.get(myChoice).equals("r") && output.get(random).equals("p")) {
+			System.out.println("あいこです");
+		}
+	}
+}
